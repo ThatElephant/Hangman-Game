1111b96fdf964e341545c32678604812bd198761
ThatElephant <That.Elephant@yahoo.com>
2017-04-01 01:25:22 -0700

    html css finished.

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+/*	1-Start
+		new word
+		hangman clears
+		answer space fixes to correct length
+	2-play
+		user pushes key
+			log it
+			highlight which key was pressed
+			change guesses +1
+			add key to either letter guessed or correct position
+			if wrong add hangman part
+	3-game
+		if win add point 
+		if loss add point 
+		new word
+		reset hangman
+		reset highlighted keys
+
+I need:
+	array for keys
+	array for words
+etc. */
